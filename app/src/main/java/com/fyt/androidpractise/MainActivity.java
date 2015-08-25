63126b2563cd0ff4f168512eea06e60475a91733
YitongFeng <fyt@YitongFengdeMacBook-Pro.local>
2015-08-25 21:33:33 +0800

    Simple ImageSwitcher 1. <ImageSwitcher /> component in main_layout.xml 2. imgSwitcher.setFactory(ViewFactory factory );    //necessary

--- app/src/main/java/com/fyt/androidpractise/MainActivity.java
+++ app/src/main/java/com/fyt/androidpractise/MainActivity.java
@@ -1,53 +1,83 @@
 package com.fyt.androidpractise;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends Activity {
-    private Button btn = null;
+    private ImageSwitcher switcher = null;
+    private Button previous = null;
+    private Button next = null;
+    private int curPic = 0;
+    private int pic[] = {R.drawable.ispic_a, R.drawable.ispic_b, R.drawable.ispic_c,
+        R.drawable.ispic_d, R.drawable.ispic_e};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_main);
-        this.btn = (Button)super.findViewById(R.id.btn);
-        this.btn.setOnClickListener(new ClickListenerImpl());
+        this.switcher = (ImageSwitcher) super.findViewById(R.id.switcher);
+        this.switcher.setFactory(new ViewFactoryImpl());
+        this.switcher.setInAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left));
+        this.switcher.setOutAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right));
+        this.switcher.setImageResource(pic[0]);
+
+        this.previous = (Button) super.findViewById(R.id.previous);
+        this.next = (Button) super.findViewById(R.id.next);
+        this.checkButEnable();
+        this.previous.setOnClickListener(new ClickListenerPrevious());
+        this.next.setOnClickListener(new ClickListenerNext());
     }
 
-    private class ClickListenerImpl implements View.OnClickListener{
+    private class ClickListenerPrevious implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            LayoutInflater factory = LayoutInflater.from(MainActivity.this);
-            View myview = factory.inflate(R.layout.login_dialog, null);
-            Dialog dialog = new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("Login")
-                    .setIcon(R.drawable.file_icon)
-                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-
-                        }
-                    })
-                    .setPositiveButton("Login", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-
-                        }
-                    })
-                    .setView(myview)
-                    .create();
-            dialog.show();
+            MainActivity.this.curPic -= 1;
+            MainActivity.this.switcher.setImageResource(pic[curPic]);
+            MainActivity.this.checkButEnable();
         }
     }
 
+    private class ClickListenerNext implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            MainActivity.this.curPic += 1;
+            MainActivity.this.switcher.setImageResource(pic[curPic]);
+            MainActivity.this.checkButEnable();
+        }
+    }
+
+    private class ViewFactoryImpl implements ViewSwitcher.ViewFactory{
+        @Override
+        public View makeView() {
+            ImageView img = new ImageView(MainActivity.this);
+            img.setBackgroundColor(0xffff);
+            img.setScaleType(ImageView.ScaleType.CENTER);
+            img.setLayoutParams(new ImageSwitcher.LayoutParams(
+                    ImageSwitcher.LayoutParams.WRAP_CONTENT, ImageSwitcher.LayoutParams.WRAP_CONTENT));
+            return img;
+        }
+    }
+
+    public void checkButEnable(){
+        if(this.curPic == 0)
+            this.previous.setEnabled(false);
+        else
+            this.previous.setEnabled(true);
+        if(this.curPic < this.pic.length - 1)
+            this.next.setEnabled(true);
+        else
+            this.next.setEnabled(false);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
